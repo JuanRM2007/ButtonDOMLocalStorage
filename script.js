3ce213413f9206d989a0816b419342864f4bf0b5
JuanM <juanm389@nycstudents.net>
2025-01-14 08:50:22 -0500

    Update script.js
    added code

--- script.js
+++ script.js
@@ -1,1 +1,4 @@
-
+const clickCountDisplay = document.getElementById('click-count');
+        const header = document.getElementById('header');
+        const button = document.getElementById('click-button');
+       
